8472f3e3114e52384af28da23599595bfde8616e
crisprking <abrahamtrueba@berkeley.edu>
2025-07-25 19:35:41 -0500

    env/requirements.txt

---
+++ lib/utils.py
@@ -0,0 +1,3 @@
+def colab_badge(repo, nb):
+    return f'[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]' \
+           f'(https://colab.research.google.com/github/{repo}/blob/main/{nb})'
